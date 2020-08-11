e02547a513d1e6817b3f31eb369f1822ef84378c
isox <theo.meunier41@gmail.com>
2020-08-11 14:15:37 +0200

    créetion upload image et delete image
    revu des message de succes et danger

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace App\Controller\Admin;
 
 use App\Entity\Option;
 use App\Entity\Property;
 use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
+use App\Services\FileServiceInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,11 +17,15 @@ class AdminPropertyController extends AbstractController
 {
     private PropertyRepository $repository;
     private EntityManagerInterface $em;
+    private FileServiceInterface $fileService;
+    private string $uploadDirectory;
 
-    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em, FileServiceInterface $fileService, string $uploadDirectory)
     {
         $this->repository = $repository;
         $this->em = $em;
+        $this->fileService = $fileService;
+        $this->uploadDirectory = $uploadDirectory;
     }
 
     /**
@@ -29,10 +35,10 @@ class AdminPropertyController extends AbstractController
     {
         $property = $this->repository->findAll();
 
-        return $this->render('admin/property/index.html.twig',[
-            'property'=>$property,
+        return $this->render('admin/property/index.html.twig', [
+            'property' => $property,
             'current_menu' => 'properties',
-            ]);
+        ]);
     }
 
     /**
@@ -44,43 +50,54 @@ class AdminPropertyController extends AbstractController
         $form = $this->createForm(PropertyType::class, $property);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid())
-        {
-           $this->em->persist($property);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $dataImage = $form->get('image')->getData();
+            if ($dataImage) {
+                $fileImage = $this->fileService->upload($dataImage, $this->uploadDirectory);
+                $property->setImage($fileImage);
+            }
+
+            $this->em->persist($property);
             $this->getDoctrine()->getManager()->flush();
             $this->addFlash('success', " Le bien été crée avec succès");
-
 
             return $this->redirectToRoute('admin.property.index');
         }
 
-        return $this->render('admin/property/new.html.twig',[
-            'property'=>$property,
+        return $this->render('admin/property/new.html.twig', [
+            'property' => $property,
             'form' => $form->createView(),
             'current_menu' => 'properties',
         ]);
     }
 
-     /**
-      * @Route("/admin/{id}", name="admin.property.edit", methods={"GET", "POST"})
+    /**
+     * @Route("/admin/{id}", name="admin.property.edit", methods={"GET", "POST"})
      */
     public function edit(Property $property, Request $request): Response
     {
         $form = $this->createForm(PropertyType::class, $property);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $dataImage = $form->get('image')->getData();
+            if ($dataImage) {
+                $fileImage = $this->fileService->upload($dataImage, $this->uploadDirectory);
+                $property->setImage($fileImage);
+            }
+
             $this->getDoctrine()->getManager()->flush();
             $this->addFlash('success', " Le bien été modifier avec succès");
 
             return $this->redirectToRoute('admin.property.index');
         }
 
-        return $this->render('admin/property/edit.html.twig',[
-            'property'=>$property,
+        return $this->render('admin/property/edit.html.twig', [
+            'property' => $property,
             'form' => $form->createView(),
             'current_menu' => 'properties',
-            ]);
+        ]);
     }
 
     /**
@@ -88,7 +105,7 @@ class AdminPropertyController extends AbstractController
      */
     public function delete(Property $property, Request $request)
     {
-        if ($this->isCsrfTokenValid('delete' .$property->getId(), $request->get('_token'))){
+        if ($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))) {
 
             $this->em->remove($property);
             $this->getDoctrine()->getManager()->flush();
@@ -96,9 +113,29 @@ class AdminPropertyController extends AbstractController
 
         }
 
-        return $this->render('admin/property/index.html.twig',[
-            'property'=>$property,
+        return $this->render('admin/property/index.html.twig', [
+            'property' => $property,
             'current_menu' => 'properties',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/delete_image/{id}", name="admin.properdy.delete_image")
+     */
+    public function deleteImage(int $id): Response
+    {
+        $property = $this->repository->find($id);
+
+        $image = $property->getImage();
+        $this->fileService->delete($image, $this->uploadDirectory);
+
+        $property->setImage(null);
+        $this->getDoctrine()->getManager()->flush();
+
+        $this->addFlash('success', " L'image été supprimer avec succès");
+
+        return $this->redirectToRoute('admin.property.edit', [
+            'id' => $property->getId(),
         ]);
     }
 }
